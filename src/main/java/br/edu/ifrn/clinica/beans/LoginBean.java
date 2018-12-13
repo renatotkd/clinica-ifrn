53523d50e77a49014c71e31951b9781d423339ea
Leandro Bezerra <leandrobezerra94@hotmail.com>
2018-12-13 10:12:38 -0300

    Login-Parcial

---
+++ src/main/java/br/edu/ifrn/clinica/beans/LoginBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.clinica.beans;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.security.auth.login.LoginException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author leandro
+ */
+@Model
+public class LoginBean {
+
+    private String email;
+    private String senha;
+
+    @Inject
+    private HttpServletRequest request;
+
+    public String realizaLogin() throws ServletException {
+
+        System.out.println("Logando...");
+        request.login(email, senha);
+        return "auth";
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
